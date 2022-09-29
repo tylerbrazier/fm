e864eae33f44f46795c7defab72b7bfdbfbe33b1
Tyler Brazier <tyler@tylerbrazier.com>
2022-09-29 22:58:37 +0700

    Got initial media player working
    On an overnight bus in Thailand

--- static/script.js
+++ static/script.js
@@ -1,20 +1,30 @@
 async function ls() {
   try {
     const route = hash()
-    const files = await api(route)
-    filesDiv().innerHTML = '' // clear existing
-    files.forEach(f => {
+    const fileNames = await api(route)
+    const filesDiv = document.querySelector('#files')
+    filesDiv.innerHTML = '' // clear existing
+    fileNames.forEach(f => {
       const fileDiv = document.createElement('div')
       const textNode = document.createTextNode(f)
+      const path = route+f
+      fileDiv.classList.add('file')
       if (f.endsWith('/')) {
+        // it's a dir
         const a = document.createElement('a')
-        a.setAttribute('href', '#'+route+f)
+        a.setAttribute('href', '#'+path)
         a.appendChild(textNode)
         fileDiv.appendChild(a)
       } else {
+        // it's a regular file
         fileDiv.appendChild(textNode)
+        fileDiv.setAttribute('data-path', path)
+        fileDiv.onclick = fileClicked
+        if (isAudioFile(path)) {
+          fileDiv.classList.add('song')
+        }
       }
-      filesDiv().appendChild(fileDiv)
+      filesDiv.appendChild(fileDiv)
     })
   } catch (err) {
     handleErr(err)
@@ -29,19 +39,19 @@ async function api(route) {
 }
 
 function handleErr(err) {
-  const errDiv = document.getElementById('error')
+  const errDiv = document.querySelector('#error')
   errDiv.innerHTML = '' // clear any existing message
-  const text = document.createTextNode(err.message)
   const x = document.createElement('button')
+  x.classList.add('x')
   x.innerText = 'X'
-  x.onclick = () => { errDiv.style.display = 'none' }
+  x.onclick = () => {
+    errDiv.style.display = 'none'
+    errDiv.innerHTML = ''
+  }
+  const text = document.createTextNode(err.message)
   errDiv.appendChild(text)
   errDiv.appendChild(x)
   errDiv.style.display = 'block' // show it
-}
-
-function filesDiv() {
-  return document.getElementById('files')
 }
 
 function hash() {
@@ -57,6 +67,51 @@ window.onhashchange = async () => {
     if (hash() !== newHash) return window.location.hash = newHash
   }
   await ls()
+}
+
+function fileClicked(event) {
+  const path = event.target.getAttribute('data-path')
+  if (isAudioFile(path)) {
+    playAudio(path)
+  }
+}
+
+function isAudioFile(path) {
+  const ext = path.split('.').pop()
+  const musicExts = [
+    'mp3', 'ogg', 'opus', 'm4a'
+  ]
+  return musicExts.includes(ext)
+}
+
+function playAudio(path) {
+  // We want to tear down the audio element when X is
+  // pressed to end the media session (notification):
+  // https://developer.mozilla.org/en-US/docs/Web/API/Media_Session_API
+  // Rather than defining some elements in the html file
+  // that will stick around and make some dynamic ones,
+  // we'll just rebuild everything when a new song is played;
+  // plus then there's not all these elements hiding in the DOM.
+  const musicDiv = document.querySelector('#music')
+  musicDiv.innerHTML = '' // remove existing
+  const titleDiv = document.createElement('div')
+  titleDiv.classList.add('title')
+  titleDiv.innerText = path
+  const audio = document.createElement('audio')
+  audio.setAttribute('controls', '')
+  audio.setAttribute('src', '/files'+path)
+  const x = document.createElement('button')
+  x.classList.add('x')
+  x.innerText = 'X'
+  x.onclick = () => {
+    musicDiv.style.display='none'
+    musicDiv.innerHTML = ''
+  }
+  musicDiv.appendChild(titleDiv)
+  musicDiv.appendChild(audio)
+  musicDiv.appendChild(x)
+  musicDiv.style.display = 'block' // show it
+  audio.play()
 }
 
 (async function() {
