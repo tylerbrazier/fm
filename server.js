afdd20640cd23a8d16a78e37fbf7d33a6836375f
Tyler Brazier <tyler@tylerbrazier.com>
2022-09-30 10:40:09 +0700

    server.js: use promises instead of callbacks
    Will make it much easier to deal with symlinks next

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('node:http')
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
 const os = require('node:os')
 
 const port = 8080
@@ -11,43 +11,39 @@ const server = http.createServer()
 server.on('request', onRequest)
 server.listen(port, () => console.log('Listening on '+port))
 
-function onRequest(req, res) {
+async function onRequest(req, res) {
   try {
     // base doesn't matter here; we just need to parse the path
-    var url = new URL(req.url, 'http://localhost')
+    const url = new URL(req.url, 'http://localhost')
+    if (!url.pathname || url.pathname === '/') {
+      await sendFile(staticDir+'/index.html', res)
+    } else if (url.pathname.startsWith(filesPrefix)) {
+      const path = root+url.pathname.slice(filesPrefix.length)
+      if (path.endsWith('/')) await ls(path, res)
+      else await sendFile(path, res)
+    } else {
+      await sendFile(staticDir+url.pathname, res)
+    }
   } catch (err) {
     return handleErr(err, res)
   }
-  if (!url.pathname || url.pathname === '/') {
-    sendFile(staticDir+'/index.html', res)
-  } else if (url.pathname.startsWith(filesPrefix)) {
-    const path = root+url.pathname.slice(filesPrefix.length)
-    if (path.endsWith('/')) ls(path, res)
-    else sendFile(path, res)
-  } else {
-    sendFile(staticDir+url.pathname, res)
-  }
 }
 
-function sendFile(filepath, res) {
-  fs.readFile(filepath, (err,data) => {
-    if (err) return handleErr(err, res)
-    const type = getContentType(filepath)
-    if (type) res.setHeader('Content-Type', type)
-    res.end(data)
-  })
+async function sendFile(filepath, res) {
+  const data = await fs.readFile(filepath)
+  const type = getContentType(filepath)
+  if (type) res.setHeader('Content-Type', type)
+  res.end(data)
 }
 
-function ls(path, res) {
+async function ls(path, res) {
   const opts = {withFileTypes:true}
-  fs.readdir(path, opts, (err, dirents) => {
-    if (err) return handleErr(err, res)
-    const files = dirents.map(d => {
-      if (d.isDirectory()) return d.name+'/'
-      else return d.name
-    })
-    sendJson(files, res)
+  const dirents = await fs.readdir(path, opts)
+  const files = dirents.map(d => {
+    if (d.isDirectory()) return d.name+'/'
+    else return d.name
   })
+  sendJson(files, res)
 }
 
 function handleErr(err, res) {
